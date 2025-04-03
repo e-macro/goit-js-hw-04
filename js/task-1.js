fde41af29b689aaa92906b512b85390dd2152f7f
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-04-03 21:23:26 +0300

    .

--- js/task-1.js
+++ js/task-1.js
@@ -14,7 +14,7 @@ function isEnoughCapacity(products, containerSize) {
 
 console.log(
   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
+); //true
 
 console.log(
   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
